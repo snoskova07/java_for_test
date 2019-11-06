79dfbca828b44caf0236a4dfd0e0430bff9df71c
Svetlana Noskova <snoskova07@gmail.com>
2019-11-06 17:58:42 +0700

    Лекция 4.8 Поиск максимального элемента в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -19,11 +20,16 @@ public class GroupModificationTests extends TestBase {
 
     app.getGroupHelper().selectGroup(before.size() - 1);
     app.getGroupHelper().initGroupModification();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1edit", "test2edit", "test3edit"));
+    GroupData group = new GroupData(before.get(before.size() - 1).getId(), "test1edit", "test2edit", "test3edit");
+    app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupModification();
     app.getGroupHelper().returnToGroupPage();
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size());
+
+    before.remove(before.size() - 1);
+    before.add(group);
+    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
 
 }
