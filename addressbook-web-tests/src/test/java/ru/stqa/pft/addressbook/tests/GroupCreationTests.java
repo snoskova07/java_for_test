5cc7b88ba76316787fa76ba0b0dc1eb320d8c647
Svetlana Noskova <snoskova07@gmail.com>
2019-11-17 17:25:40 +0700

    Лекция 6.5. Загрузка тестовых данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import ru.stqa.pft.addressbook.model.GroupData;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -14,13 +15,16 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
       List<Object[]> list = new ArrayList<Object[]>();
-      list.add(new Object[]{new GroupData().withName("test 1").withHeader("header 1").withFooter("footer 1")});
-      list.add(new Object[]{new GroupData().withName("test 2").withHeader("header 2").withFooter("footer 2")});
-      list.add(new Object[]{new GroupData().withName("test 3").withHeader("header 3").withFooter("footer 3")});
-
-        return list.iterator();
+      BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+      String line = reader.readLine();
+      while (line != null) {
+        String[] split = line.split(";");
+        list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+        line = reader.readLine();
+      }
+      return list.iterator();
     }
 
     @Test (dataProvider = "validGroups")
@@ -36,7 +40,7 @@ public class GroupCreationTests extends TestBase {
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test
+    @Test (enabled = false)
     public void testBadGroupCreation() throws Exception {
         app.goTo().groupPage();
         Groups before = app.group().all();
