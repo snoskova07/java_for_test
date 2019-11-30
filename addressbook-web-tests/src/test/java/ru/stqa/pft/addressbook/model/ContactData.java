1003c3509449741a0e2e1e23a3ef7fde0a31d37c
Svetlana Noskova <snoskova07@gmail.com>
2019-11-30 15:57:27 +0700

    Лекция 7.2. Object Relational Mapping (ORM)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,38 +3,65 @@ package ru.stqa.pft.addressbook.model;
 import com.google.gson.annotations.Expose;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
 @XStreamAlias("contact")
 
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name="id")
   private int id = Integer.MAX_VALUE;
   @Expose
+  @Column(name="firstname")
   private String firstName;
   @Expose
+  @Column(name="lastname")
   private String lastName;
   @Expose
+  @Column(name="address")
+  @Type(type="text")
   private String address;
   @Expose
+  @Column(name="email")
+  @Type(type="text")
   private String email;
   @Expose
+  @Column(name="home")
+  @Type(type="text")
   private String home;
   @Expose
+  @Column(name="mobile")
+  @Type(type="text")
   private String mobile;
   @Expose
+  @Column(name="work")
+  @Type(type="text")
   private String work;
+  @Transient
   private String allPhones;
   @Expose
+  @Column(name="email2")
+  @Type(type="text")
   private String email2;
   @Expose
+  @Column(name="email3")
+  @Type(type="text")
   private String email3;
+  @Transient
   private String allEmails;
   @Expose
+  @Transient
   private String group;
   @Expose
-  private File photo;
+  @Column(name="photo")
+  @Type(type="text")
+  private String photo;
 
   public ContactData withId(int id) {
     this.id = id;
@@ -102,7 +129,7 @@ public class ContactData {
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
@@ -159,7 +186,7 @@ public class ContactData {
   }
 
   public File getPhoto() {
-    return photo;
+    return new File(photo);
   }
 
   @Override
