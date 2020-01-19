57de674d655e68a9b35571e21690415b021e6fd4
Svetlana Noskova <snoskova07@gmail.com>
2020-01-19 15:15:12 +0700

    Задание 20 Исправленное

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestAssuredTests.java
@@ -28,7 +28,7 @@ public class RestAssuredTests  {
     }
 
     private Set<Issue> getIssues(){
-        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json").asString();
+        String json = RestAssured.get("https://bugify.stqa.ru/api/issues.json?limit=500").asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
         return new Gson().fromJson(issues, new TypeToken<Set<Issue>>(){}.getType());
@@ -38,7 +38,7 @@ public class RestAssuredTests  {
         String json = RestAssured.given()
                 .parameter("subject", newIssue.getSubject())
                 .parameter("description", newIssue.getDescription())
-                .post("https://bugify.stqa.ru/api/issues.json").asString();
+                .post("https://bugify.stqa.ru/api/issues.json?limit=500").asString();
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
     }
