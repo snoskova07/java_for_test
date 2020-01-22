75bd0e13c2b5f35f5e885d913357497700e1d944
Svetlana Noskova <snoskova07@gmail.com>
2020-01-22 17:55:09 +0700

    Поменяла браузер на хром

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -22,7 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TestBase {
   Logger logger = LoggerFactory.getLogger(TestBase.class);
-  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.FIREFOX));
+  protected static final ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() throws Exception {
